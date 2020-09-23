067b33dc08322aa767e5641612ff5c652142c4ab
Aman Singla <58655611+AmanMatrix@users.noreply.github.com>
2020-09-24 00:32:21 +0530

    Delete using selnium driver.py

--- Personal/using selnium driver.py
+++
@@ -1,7 +0,0 @@
-from selenium import webdriver
-
-DRIVER_PATH = r'C:\Users\Aman\Desktop\StudyM\Python\Chrome automater\chromedriver.exe'
-driver = webdriver.Chrome(executable_path=DRIVER_PATH)
-driver.get('https://google.com')
-https://meet.google.com/gbh-nmjn-jzb
-
